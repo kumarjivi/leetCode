a53aac3fba7d6e364271d2cb26c6e8845a44d4c6
Kumar Jivi <kumarjivi@yahoo.com>
2018-07-04 13:54:04 -0700

    Added solution to problem 22 'Generate Parentheses'

---
+++ LeetCode/src/problems1to50/GenerateParentheses22.java
@@ -0,0 +1,47 @@
+package problems1to50;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+/***
+ * Given n pairs of parentheses,
+ * write a function to generate all combinations of well-formed parentheses.
+ * For example, given n = 3, a solution set is:
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ * @author jivi
+ *	
+ */
+public class GenerateParentheses22 {
+
+	public static void main(String[] args) {
+		List<String> output = generateParenthesis(4);
+		output.forEach(s->System.out.println(s));
+	}
+	public static List<String> generateParenthesis(int n) {
+        List<String> output = new ArrayList<String>();
+        int open = 0, close = 0;
+        helper(output, "", open, close, n);
+        return output;
+    }
+    
+    private static void helper(List<String> output, String soFar, int open, int close, int n) {
+        if(open == n && close == n) {
+            output.add(soFar);
+            return;
+        }
+        if(open < n) {
+            helper(output, soFar+"(", open+1, close, n);
+        }
+        if(close < open) {
+            helper(output, soFar+")", open, close+1, n);
+        }
+    }
+}
